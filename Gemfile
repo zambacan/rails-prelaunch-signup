4c36d8bf705e1c16654df98e402784b7e400000a
Raife Stokes <raifestokes@gmail.com>
2014-05-02 15:25:47 +1000

    getting fonts working

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ group :production do
   gem 'pg', "0.17.0"
 end
 group :assets do
-  gem 'sass-rails',   '~> 3.2.3'
+  gem 'sass-rails'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
   gem 'font-awesome-rails'
--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module RailsPrelaunchSignup
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
 
-
+    # config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
